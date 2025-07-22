4a555c19de5c0dcdf2225bda3431c9d4de4d3fee
JackCh6366 <jack.chen63662@gmail.com>
2025-07-22 09:31:50 +0800

    update

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,10 +1,11 @@
 from playwright.async_api import async_playwright
 import asyncio
 
+
 async def main():
     async with async_playwright() as p:
         #browser = await p.chromium.launch(headless=False)
-        browser = await p.firefox.launch(headless=False,slow_mo=10000)  # Use Firefox instead of Chromium
+        browser = await p.chromium.launch(headless=False, slow_mo=10000)
         page = await browser.new_page()
         await page.goto('https://example.com')
         await page.wait_for_selector('p') #等待元素載入
